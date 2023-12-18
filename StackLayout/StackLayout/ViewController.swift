c1d77d2925004899a1b0b111b76eef7479371f98
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-12-18 18:57:28 +0300

    add stack layout

---
+++ StackLayout/StackLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackLayout
+//
+//  Created by Berkay Çaylı on 18.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
